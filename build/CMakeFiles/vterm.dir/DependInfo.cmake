e745082163ef67f06325828c0e28bf9eb9e0a75b
cy384 <cy384@cy384.com>
2020-09-06 14:45:28 -0400

    init from 0.1.3 with retro68 cmakelist

---
+++ build/CMakeFiles/vterm.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/palomides/src/libvterm/libvterm-0.1.3/src/encoding.c" "/home/palomides/src/libvterm/libvterm-0.1.3/build/CMakeFiles/vterm.dir/src/encoding.c.obj"
+  "/home/palomides/src/libvterm/libvterm-0.1.3/src/keyboard.c" "/home/palomides/src/libvterm/libvterm-0.1.3/build/CMakeFiles/vterm.dir/src/keyboard.c.obj"
+  "/home/palomides/src/libvterm/libvterm-0.1.3/src/mouse.c" "/home/palomides/src/libvterm/libvterm-0.1.3/build/CMakeFiles/vterm.dir/src/mouse.c.obj"
+  "/home/palomides/src/libvterm/libvterm-0.1.3/src/parser.c" "/home/palomides/src/libvterm/libvterm-0.1.3/build/CMakeFiles/vterm.dir/src/parser.c.obj"
+  "/home/palomides/src/libvterm/libvterm-0.1.3/src/pen.c" "/home/palomides/src/libvterm/libvterm-0.1.3/build/CMakeFiles/vterm.dir/src/pen.c.obj"
+  "/home/palomides/src/libvterm/libvterm-0.1.3/src/screen.c" "/home/palomides/src/libvterm/libvterm-0.1.3/build/CMakeFiles/vterm.dir/src/screen.c.obj"
+  "/home/palomides/src/libvterm/libvterm-0.1.3/src/state.c" "/home/palomides/src/libvterm/libvterm-0.1.3/build/CMakeFiles/vterm.dir/src/state.c.obj"
+  "/home/palomides/src/libvterm/libvterm-0.1.3/src/unicode.c" "/home/palomides/src/libvterm/libvterm-0.1.3/build/CMakeFiles/vterm.dir/src/unicode.c.obj"
+  "/home/palomides/src/libvterm/libvterm-0.1.3/src/vterm.c" "/home/palomides/src/libvterm/libvterm-0.1.3/build/CMakeFiles/vterm.dir/src/vterm.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/cmake_install.cmake
@@ -0,0 +1,55 @@
+# Install script for directory: /home/palomides/src/libvterm/libvterm-0.1.3
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/home/palomides/src/Retro68-build/toolchain/m68k-apple-macos")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE FILE FILES
+    "/home/palomides/src/libvterm/libvterm-0.1.3/include/vterm.h"
+    "/home/palomides/src/libvterm/libvterm-0.1.3/include/vterm_keycodes.h"
+    )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/home/palomides/src/libvterm/libvterm-0.1.3/build/libvterm.a")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/home/palomides/src/libvterm/libvterm-0.1.3/build/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
